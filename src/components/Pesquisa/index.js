dbccb5fbd81a83ff47da6d5bb19c9bbc04e1c291
Thiago Cordeiro <thiagocordeirodantasp@gmail.com>
2023-07-24 12:56:48 -0300

    Utilizando o UseEffect

--- src/components/Pesquisa/index.js
+++ src/components/Pesquisa/index.js
@@ -1,7 +1,9 @@
 import Input from '../Input'
 import styled from 'styled-components'
 import { useState } from 'react'
-import { livros } from './dadosPesquisa'
+import { useEffect } from 'react'
+import { getLivros } from '../../services/livro'
+
 
 const PesquisaContainer = styled.section`
     background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
@@ -47,6 +49,13 @@ const Resultado = styled.div`
 
 function Pesquisa() {
     const [livrosPesquisados, setLivrosPesquisados] = useState([])
+    const [livros, setLivros] = useState([])
+
+    useEffect(() => {
+       const livrosDaAPI =  getLivros()
+       setLivros(livrosDaAPI)
+    }, [])
+
 
     return (
         <PesquisaContainer>
